7a9975cd065a11f779a90e27d834953054dc4016
Habib Ali <admin@admins-mac-mini.local>
2018-06-05 07:57:57 +0500

    Add Data Manipulation Methods (saveItems and loadItems) for saving and loading items

--- TodoAppRevise/TodoTableViewController.swift
+++ TodoAppRevise/TodoTableViewController.swift
@@ -11,26 +11,28 @@ import CoreData
 
 class TodoTableViewController: UITableViewController {
 
-    let itemArray = ["Revise CoreData"]
+    var itemArray = [Todo_item]()
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
-   let item = Parrent_todo(context: (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext)
+//   let item = Parrent_todo(context: (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext)
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //tableView.reloadData()
         // Do any additional setup after loading the view, typically from a nib.
-        
+        loadItems()
     }
     
     //MARK - TableView DataSource Methods:
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return itemArray.count
+       return itemArray.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "itemCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
+        cell.textLabel?.text = itemArray[indexPath.row].name
+        
         return cell
     }
     
@@ -42,16 +44,58 @@ class TodoTableViewController: UITableViewController {
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
-
-//
-//    func saveItems(){
-//        do{
-//            try context.save()
-//        }
-//        catch{
-//            print("Error saving data \(error)")
-//        }
-//    }
+    //MARK - Add New Item
+    
+    @IBAction func addNewTodo(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add New Todo Item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Todo", style: .default) { (action) in
+            //once Add Todo button is clicked
+            
+            let newTodo = Todo_item(context: self.context)
+            newTodo.name = textField.text!
+            print(textField.text!)
+            newTodo.done = false
+            
+            self.itemArray.append(newTodo)
+            print("Saving Data")
+            self.saveItems()
+            
+        }
+        alert.addAction(action)
+        alert.addTextField { (alertTextInputFeild) in
+            alertTextInputFeild.placeholder = "Create new Todo"
+            textField = alertTextInputFeild
+        }
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    //MARK - Model manuplation methodd
+ 
+    func saveItems(){
+        do{
+            try context.save()
+            print("TODO ADDED")
+        }
+        catch{
+            print("Error saving data ::: \(error)")
+        }
+        tableView.reloadData()
+    }
+    
+    func loadItems(){
+        let request : NSFetchRequest<Todo_item> = Todo_item.fetchRequest()
+        
+        do{
+            itemArray = try context.fetch(request)
+            print("CCCCCC \(itemArray.count)")
+        }catch{
+            print("Can not read Data: \(error)")
+        }
+        
+    }
 
 }
 
